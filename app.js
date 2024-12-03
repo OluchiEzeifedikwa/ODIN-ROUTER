c5fc8917f524731097aee00498f2c6ed3db6be10
OluchiEze <oluchiezeifedikwa@gmail.com>
2024-12-03 17:43:24 +0100

    Create views folder, index.ejs file, then include the navbar and user templates from the navbar.ejs and user.ejs files created in the views folder for rendering

--- app.js
+++ app.js
@@ -3,13 +3,39 @@ const app = express();
 const authorRouter = require('./routes/authorRouter');
 const bookRouter = require('./routes/bookRouter');
 const indexRouter = require('./routes/indexRouter');
+const path = require('node:path');
+
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+// app.js
+const links = [
+    { href: "/", text: "Home" },
+    { href: "about", text: "About" },
+  ];
+
+const users = ["Rose", "Cake", "Biff"];
+  
+  app.get("/", (req, res) => {
+    res.render("index", { links: links, users: users });
+  });
+  
+
+
+app.get('/', (req, res) => {
+    res.render('index', {message: 'EJS rocks'});
+});
+
+
+
 
 
 app.use('/author', authorRouter);
 app.use('/book', bookRouter);
 app.use('/', indexRouter);
-
-
 
 app.use((err, req, res, next) => {
     console.error(err);
@@ -18,6 +44,7 @@ app.use((err, req, res, next) => {
 
     // next(err);
 });
+
 
 
 const PORT = 5000;
